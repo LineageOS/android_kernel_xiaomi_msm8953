2d258f70df3665eeda6bed0451459c19ac94fea3
Padma, Santhosh Kumar <skpadma@qti.qualcomm.com>
2014-12-22 19:55:59 +0530

    prima: Change log module for EAPOL and DHCP
    For STA interface, EAPOL and DHCP prints are associated with the
    module VOS_MODULE_ID_HDD_DATA. By default, logger app logs doesn't
    give logs related to VOS_MODULE_ID_HDD_DATA. Change this module to
    VOS_MODULE_ID_HDD to get EAPOL and DHCP prints for STA interface
    in logger app.
    
    Change-Id: I4f32f65e9e7ec14b22999705f15084e680aaee2a
    CRs-Fixed: 774377

--- CORE/HDD/src/wlan_hdd_tx_rx.c
+++ CORE/HDD/src/wlan_hdd_tx_rx.c
@@ -1673,12 +1673,12 @@ VOS_STATUS hdd_tx_fetch_packet_cbk( v_VOID_t *vosContext,
                                           pHddCtx->cfg_ini->gEnableDebugLog);
       if (VOS_PKT_PROTO_TYPE_EAPOL & proto_type)
       {
-         VOS_TRACE(VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_INFO,
+         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                    "STA TX EAPOL");
       }
       else if (VOS_PKT_PROTO_TYPE_DHCP & proto_type)
       {
-         VOS_TRACE(VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_INFO,
+         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                    "STA TX DHCP");
       }
    }
@@ -2033,12 +2033,12 @@ VOS_STATUS hdd_rx_packet_cbk( v_VOID_t *vosContext,
                                              pHddCtx->cfg_ini->gEnableDebugLog);
          if (VOS_PKT_PROTO_TYPE_EAPOL & proto_type)
          {
-            VOS_TRACE(VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_INFO,
+            VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                       "STA RX EAPOL");
          }
          else if (VOS_PKT_PROTO_TYPE_DHCP & proto_type)
          {
-            VOS_TRACE(VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_INFO,
+            VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO,
                       "STA RX DHCP");
          }
       }
