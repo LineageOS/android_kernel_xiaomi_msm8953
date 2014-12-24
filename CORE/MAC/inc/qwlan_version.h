b3cb3c363934601c8b6a29243eae34c07172ad22
Satyanarayana Dash <c_sadash@qti.qualcomm.com>
2014-12-24 11:33:27 +0530

    Wlan: Release 3.2.3.483
    Release 3.2.3.483
    
    Change-Id: Ia626651925a15db8b368cdc219ec608ebc245278
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -47,8 +47,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            3
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            482
+#define QWLAN_VERSION_BUILD            483
 
-#define QWLAN_VERSIONSTR               "3.2.3.482"
+#define QWLAN_VERSIONSTR               "3.2.3.483"
 
 #endif /* QWLAN_VERSION_H */
