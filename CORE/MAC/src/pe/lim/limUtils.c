5696b73ac4b1deccbeb758148ab470a54d7f263e
Abhishek Singh <absingh@qti.qualcomm.com>
2015-01-16 10:51:45 +0530

    wlan: Delete STA after SA query timeout
    Delete the STA entry if the associated STA does not
    respond to SA Query in the configured timeout value.
    
    CRs-Fixed: 782438
    Change-Id: I33cfad91d3524597fada64a046ad72a97e94132f

--- CORE/MAC/src/pe/lim/limUtils.c
+++ CORE/MAC/src/pe/lim/limUtils.c
@@ -8065,7 +8065,13 @@ void limPmfSaQueryTimerHandler(void *pMacGlobal, tANI_U32 param)
     pSta->pmfSaQueryRetryCount++;
     if (pSta->pmfSaQueryRetryCount >= maxRetries)
     {
-        limLog(pMac, LOG1, FL("SA Query timed out"));
+        limLog(pMac, LOGE,
+           FL("SA Query timed out,Deleting STA: " MAC_ADDRESS_STR),
+                                     MAC_ADDR_ARRAY(pSta->staAddr));
+        limSendDisassocMgmtFrame(pMac,
+                  eSIR_MAC_DISASSOC_DUE_TO_INACTIVITY_REASON,
+                  pSta->staAddr, psessionEntry, FALSE);
+        limTriggerSTAdeletion(pMac, pSta, psessionEntry);
         pSta->pmfSaQueryState = DPH_SA_QUERY_TIMED_OUT;
         return;
     }
