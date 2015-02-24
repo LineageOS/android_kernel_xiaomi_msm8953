07050f5e7d2bab52196773b15dda470c05dae697
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-02-24 11:36:12 +0530

    wlan: Initialize 'staInfo_lock' before accessing.
    staInfo_lock lock is initialized during change_interface or while
    opening SAP/P2P-GO adapter. Hence in SSR scenario staInfo_lock will
    not be initialized, leading to accessing lock without initialization.
    
    Changes are done to initialize lock in WLANSAP_Start() so that lock
    will be initialized in both re_init(SSR) and driver loading.
    
    Change-Id: If71b2748e02f9fbb3e03cdee8dc5a313e142e5bb
    CRs-Fixed: 799349

--- CORE/HDD/src/wlan_hdd_softap_tx_rx.c
+++ CORE/HDD/src/wlan_hdd_softap_tx_rx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -821,8 +821,6 @@ VOS_STATUS hdd_softap_init_tx_rx( hdd_adapter_t *pAdapter )
    pAdapter->aTxQueueLimit[WLANTL_AC_BE] = HDD_SOFTAP_TX_BE_QUEUE_MAX_LEN;
    pAdapter->aTxQueueLimit[WLANTL_AC_VI] = HDD_SOFTAP_TX_VI_QUEUE_MAX_LEN;
    pAdapter->aTxQueueLimit[WLANTL_AC_VO] = HDD_SOFTAP_TX_VO_QUEUE_MAX_LEN;
-
-   spin_lock_init( &pSapCtx->staInfo_lock );
 
    for (STAId = 0; STAId < WLAN_MAX_STA_COUNT; STAId++)
    {
