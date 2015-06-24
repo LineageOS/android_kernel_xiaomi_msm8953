ca6ec5d6d60432f8dcb81f84578f8f2901a0848b
Abhishek Singh <absingh@qti.qualcomm.com>
2015-06-24 13:13:27 +0530

    wlan: update htCapabilty properly in PE session from dot11mode.
    htCapabilty in PE session is set from dot11mode, even before dot11mode
    is set.By default dot11mode is 0 (WNI_CFG_DOT11_MODE_ALL) and thus
    htCapabilty is always set to 1, even if dot11mode does not indicate
    HT or AP does not support HT.
    
    To solve this fill the htCapabilty once dot11mode is updated properly.
    
    CRs-Fixed: 860543
    Change-Id: I9228a4e171dfee757b1be71eff440b91bcea63ba

--- CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -1818,7 +1818,6 @@ __limProcessSmeJoinReq(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
             }
         }
 
-        handleHTCapabilityandHTInfo(pMac, psessionEntry);
         psessionEntry->isAmsduSupportInAMPDU = pSmeJoinReq->isAmsduSupportInAMPDU;
 
         /* Store Session related parameters */
@@ -1930,6 +1929,7 @@ __limProcessSmeJoinReq(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
         /*Phy mode*/
         psessionEntry->gLimPhyMode = pSmeJoinReq->bssDescription.nwType;
 
+        handleHTCapabilityandHTInfo(pMac, psessionEntry);
         /* Copy The channel Id to the session Table */
         psessionEntry->currentOperChannel = pSmeJoinReq->bssDescription.channelId;
         psessionEntry->htSupportedChannelWidthSet = (pSmeJoinReq->cbMode)?1:0; // This is already merged value of peer and self - done by csr in csrGetCBModeFromIes
