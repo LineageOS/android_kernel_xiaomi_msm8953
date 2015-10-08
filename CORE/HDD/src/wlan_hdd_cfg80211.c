0c11d82172146c9b6198d10741b430366e686a3d
Mahesh A Saptasagar <c_msapta@qti.qualcomm.com>
2015-10-08 19:54:08 +0530

    wlan: Disable isPnoEnable flag if sched_scan_stop fails due to SSR
    If sched scan stop fails due to SSR, further attempts to enable
    PNO will fail as isPnoEnable flag is not cleared as a part of sched
    scan stop. Also Since SSR is triggered firmware will not remember the
    earlier PNO configuration, which will end up with no scans in suspend
    mode.
    
    Change-Id: I0e172d16a7f9d67dd39bf92abc96eca90bb07b61
    CRs-Fixed: 920874

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -16736,6 +16736,7 @@ static int __wlan_hdd_cfg80211_sched_scan_stop(struct wiphy *wiphy,
     {
         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
                   "%s: LOGP in Progress. Ignore!!!", __func__);
+        pHddCtx->isPnoEnable = FALSE;
         return ret;
     }
 
