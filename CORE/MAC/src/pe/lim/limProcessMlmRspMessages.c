c0fccd210e08f6b8a9e89fcd62d5e6eb4eee181a
Abhishek Singh <absingh@qti.qualcomm.com>
2015-10-19 16:44:00 +0530

    Wlan: Update the protStatusCode in Re/assoc response in case of failure
    If deauth is received just after Assoc rsp from AP in addSTA
    response Re/assoc confirm is sent with status failure.
    But protStatusCode in assoc confirm is not updated and thus the
    pRoamInfo->reasonCode may have garbage value.
    In case of Wep connection failure reason code,
    pRoamInfo->reasonCode, is sent to the supplicant in connect
    result. Now if pRoamInfo->reasonCode is 0 supplicant receives
    connection success though connection have actually failed.
    
    To avoid this update protStatusCode, before sending Re/assoc
    failure.
    Also properly derive if connection is WEP from roamProfile before
    clearing it.
    
    CRs-Fixed: 926572
    Change-Id: I5ae0832d37746be16696f521bddd5b67307b1ee4

--- CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
+++ CORE/MAC/src/pe/lim/limProcessMlmRspMessages.c
@@ -1985,6 +1985,7 @@ void limProcessStaMlmAddStaRsp( tpAniSirGlobal pMac, tpSirMsgQ limMsgQ ,tpPESess
           //either assoc cnf or reassoc cnf handler.
           mlmAssocCnf.resultCode =
               (tSirResultCodes) eSIR_SME_JOIN_DEAUTH_FROM_AP_DURING_ADD_STA;
+          mlmAssocCnf.protStatusCode = eSIR_MAC_UNSPEC_FAILURE_STATUS;
           psessionEntry->staId = pAddStaParams->staIdx;
           goto end;
       }
@@ -2056,6 +2057,7 @@ void limProcessStaMlmAddStaRsp( tpAniSirGlobal pMac, tpSirMsgQ limMsgQ ,tpPESess
         if(psessionEntry->limSmeState == eLIM_SME_WT_REASSOC_STATE)
            mesgType = LIM_MLM_REASSOC_CNF;
         mlmAssocCnf.resultCode = (tSirResultCodes) eSIR_SME_REFUSED;
+        mlmAssocCnf.protStatusCode = eSIR_MAC_UNSPEC_FAILURE_STATUS;
     }
 end:
     if( 0 != limMsgQ->bodyptr )
@@ -3271,6 +3273,7 @@ limProcessStaMlmAddBssRsp( tpAniSirGlobal pMac, tpSirMsgQ limMsgQ,tpPESession ps
     {
         limLog( pMac, LOGP, FL( "SessionId:%d ADD_BSS failed! mlmState = %d" ),
                 psessionEntry->peSessionId,  psessionEntry->limMlmState);
+        mlmAssocCnf.protStatusCode = eSIR_MAC_UNSPEC_FAILURE_STATUS;
         // Return Assoc confirm to SME with failure
         if(eLIM_MLM_WT_ADD_BSS_RSP_FT_REASSOC_STATE == psessionEntry->limMlmState)
             mlmAssocCnf.resultCode = (tSirResultCodes) eSIR_SME_FT_REASSOC_FAILURE;
