f79625899c57b73323aaa364a7d7b972ad33530c
Abhishek Singh <absingh@qti.qualcomm.com>
2015-10-23 10:54:06 +0530

    Wlan: Change to avoid indicating disconnect event to kernel
    qcacld-2.0 to pronto propagation.
    
    Currently as part of reassociate to different BSS in same ESS
    supplicant will issue connect command in connected state.
    As part of above connect attempt driver disconnects to current AP
    and associates to new AP by indicating the disconnect and connect
    indications to kernel.
    
    Kernel on receiving disconnect indication from driver resets the
    wdev ssid length parameters which is updated on receiving connect
    command from supplicant.
    
    Kernel on receiving connect result indication from driver tries
    to fetch the BSS information from local scan results and compares
    the SSID but since we reset the ssid length in wdev it couldn't
    find the BSS resulting in various issues.
    
    Address the above issue by not indicating disconnect event to
    kernel on receiving connect in already connected state.
    
    Change-Id: I73746a53ed69c1d728da9fa48af75f5cad50fa8c
    CRs-Fixed: 898494

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -14307,6 +14307,8 @@ static int wlan_hdd_try_disconnect( hdd_adapter_t *pAdapter )
       (eConnectionState_Associated == pHddStaCtx->conn_info.connState) ||
       (eConnectionState_IbssConnected == pHddStaCtx->conn_info.connState))
     {
+        hdd_connSetConnectionState(pHddStaCtx,
+                     eConnectionState_Disconnecting);
         /* Issue disconnect to CSR */
         INIT_COMPLETION(pAdapter->disconnect_comp_var);
         if( eHAL_STATUS_SUCCESS ==
