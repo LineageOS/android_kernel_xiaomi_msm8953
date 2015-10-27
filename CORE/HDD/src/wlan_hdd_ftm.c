188c67a79f9145789540031be9af2515d34cb666
Mukul Sharma <mukul@qti.qualcomm.com>
2015-10-27 23:08:35 +0530

    wlan: ensure permission for WLAN_FTM_PRIV_SET_VAR_INT_GET_NONE
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing WLAN_FTM_PRIV_SET_VAR_INT_GET_NONE
    making sure user task has right permission to process the command.
    
    Change-Id: I9d10241b0dbcc1839eb0f6907aa2bf233359e928
    CRs-Fixed: 930988

--- CORE/HDD/src/wlan_hdd_ftm.c
+++ CORE/HDD/src/wlan_hdd_ftm.c
@@ -5686,6 +5686,13 @@ static int iw_ftm_set_var_ints_getnone(struct net_device *dev, struct iw_request
    int apps_args[MAX_VAR_ARGS] = {0};
    int num_args;
 
+    if (!capable(CAP_NET_ADMIN))
+    {
+         VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+            FL("permission check failed"));
+         return -EPERM;
+    }
+
     /* helper function to get iwreq_data with compat handling. */
     if (hdd_priv_get_data(&u_priv_wrqu.data, wrqu))
     {
