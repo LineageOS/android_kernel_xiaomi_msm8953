d75a8d80cae5c639b149e96c42277e61e1eaa410
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2015-10-27 21:55:28 +0530

    wlan:Check priviledge permission before processing SET_PACKET_FILTER IOCTL
    Kernel assumes all SET IOCTL commands are assigned with even
    numbers. But in our WLAN driver, some SET IOCTLS are assigned with
    odd numbers. This leads kernel fail to check, for some SET IOCTLs,
    whether user has the right permission to do SET operation.
    Hence, in driver, before processing SET_PACKET_FILTER IOCTL, making
    sure user task has right permission to process the command.
    
    Change-Id: Ib49c3223eacdc90dfe0d45af1aff7c74518990df
    CRs-Fixed: 930937

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -8845,6 +8845,14 @@ static int __iw_set_packet_filter_params(struct net_device *dev,
     struct iw_point s_priv_data;
 
     ENTER();
+
+    if (!capable(CAP_NET_ADMIN))
+    {
+        VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_ERROR,
+                  FL("permission check failed"));
+        return -EPERM;
+    }
+
     pAdapter = WLAN_HDD_GET_PRIV_PTR(dev);
     if (NULL == pAdapter)
     {
