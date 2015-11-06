02c16e82828d0c20580df36b54470a6d66b72efb
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2015-11-06 14:42:25 +0530

    Wlan: Release 3.0.10.180
    Release 3.0.10.180
    
    Change-Id: Ia54d1cb3e0aaf69af574746c295a0a3d3aee4ccf
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,8 +44,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            179
+#define QWLAN_VERSION_BUILD            180
 
-#define QWLAN_VERSIONSTR               "3.0.10.179"
+#define QWLAN_VERSIONSTR               "3.0.10.180"
 
 #endif /* QWLAN_VERSION_H */
