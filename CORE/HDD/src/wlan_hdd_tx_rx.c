18c3c6730432c54896e29ac4fc3ef844b3288c1c
Ganesh Kondabattini <ganeshk@qti.qualcomm.com>
2015-11-27 15:08:40 +0530

    wlan: Don't aquire mutex lock in softirq context
    __hdd_tx_timeout is getting called in softirq context and driver
    is trying hold mutex in this function.
    
    Avoid holding mutex in softirq context.
    
    Change-Id: I532a3ec4d090bd1e35c1c1e7558b10b4f0c44dc3
    CRs-Fixed: 944887

--- CORE/HDD/src/wlan_hdd_tx_rx.c
+++ CORE/HDD/src/wlan_hdd_tx_rx.c
@@ -1181,11 +1181,16 @@ void __hdd_tx_timeout(struct net_device *dev)
                 "%s: Request firmware for recovery",__func__);
       WLANTL_TLDebugMessage(WLANTL_DEBUG_FW_CLEANUP);
    }
-   mutex_lock(&pHddCtx->roc_lock);
+
+   /*
+    * This function is getting called in softirq context, So don't hold
+    * any mutex.
+    * There is no harm here in not holding the mutex as long as we are
+    * not accessing the pRemainChanCtx contents.
+    */
    pRemainChanCtx = hdd_get_remain_on_channel_ctx(pHddCtx);
    if (!pRemainChanCtx)
    {
-       mutex_unlock(&pHddCtx->roc_lock);
       if (pAdapter->hdd_stats.hddTxRxStats.continuousTxTimeoutCount >
           HDD_TX_STALL_SSR_THRESHOLD)
       {
@@ -1201,7 +1206,6 @@ void __hdd_tx_timeout(struct net_device *dev)
    }
    else
    {
-       mutex_unlock(&pHddCtx->roc_lock);
       VOS_TRACE(VOS_MODULE_ID_HDD_DATA, VOS_TRACE_LEVEL_ERROR,
                 "Remain on channel in progress");
       /* The supplicant can retry "P2P Invitation Request" for 120 times
