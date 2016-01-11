b7f449ffd93df48f9001169e488fdcf650a0fd89
Padma, Santhosh Kumar <skpadma@qti.qualcomm.com>
2016-01-11 18:55:44 +0530

    prima: RTT3: Addition of self sta address in interface
    Self sta address is required for firmware to identify actual
    interface for STA. Add self sta address variable in RTT3 HAL
    request.
    
    Change-Id: I5fdddc3ebc7d84d0d290b44df3186b27524c09e6
    CRs-Fixed: 960462

--- riva/inc/wlan_hal_msg.h
+++ riva/inc/wlan_hal_msg.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016 The Linux Foundation. All rights reserved.
 * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
 
 *
@@ -2902,6 +2902,8 @@ WLAN_HAL_START_OEM_DATA_REQ_IND_NEW------------------------------------
 --------------------------------------------------------------------------*/
 typedef PACKED_PRE struct PACKED_POST
 {
+   tSirMacAddr selfMacAddr;
+   tANI_U8 reserved[2];
    tANI_U8   oemDataReq[NEW_OEM_DATA_REQ_SIZE];
 } tStartOemDataReqParamsNew, *tpStartOemDataReqParamsNew;
 
