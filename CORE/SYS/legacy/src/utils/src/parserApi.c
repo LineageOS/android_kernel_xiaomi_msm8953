8a2516038419c68fe0124fca807de3d9d04ef6e7
Sreelakshmi Konamki <c_skonam@qti.qualcomm.com>
2016-02-19 16:12:23 +0530

    wlan: Add presence check for structure tDot11fQosMapConfigure
    qcacld-2.0 to prima propagation
    
    Current logic use the "not present" structure tDot11fQosMapConfigure
    to calculate the array index, which lead to the array overflow and
    crash of VosMCThread. Add check code to avoid this problem.
    
    Change-Id: Ifa08b24a06e409fed1bd445574127fef02f21d19
    CRs-Fixed: 978582

--- CORE/SYS/legacy/src/utils/src/parserApi.c
+++ CORE/SYS/legacy/src/utils/src/parserApi.c
@@ -4265,9 +4265,9 @@ sirConvertQosMapConfigureFrame2Struct(tpAniSirGlobal    pMac,
     tDot11fQosMapConfigure mapConfigure;
     tANI_U32 status;
     status = dot11fUnpackQosMapConfigure(pMac, pFrame, nFrame, &mapConfigure);
-    if ( DOT11F_FAILED( status ) )
+    if ( DOT11F_FAILED( status ) || !mapConfigure.QosMapSet.present )
     {
-        dot11fLog(pMac, LOGE, FL("Failed to parse Qos Map Configure frame (0x%08x, %d bytes):"),
+        dot11fLog(pMac, LOGE, FL("Failed to parse or QosMapSet not present(0x%08x, %d bytes):"),
                   status, nFrame);
         PELOG2(sirDumpBuf(pMac, SIR_DBG_MODULE_ID, LOG2, pFrame, nFrame);)
         return eSIR_FAILURE;
