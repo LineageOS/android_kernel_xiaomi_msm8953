bb75a14bc255be472edd591d29aaf699ae59bb73
Padma, Santhosh Kumar <skpadma@qti.qualcomm.com>
2016-02-25 18:36:46 +0530

    prima: Fix NULL pointer dereference
    Variable pRoamInfo can be NULL. Add NULL check for pRoamInfo
    before dereferencing to avoid potential issues.
    
    Change-Id: I2be728845571de899f005b560c845490e05cd3bd
    CRs-Fixed: 981762

--- CORE/HDD/src/wlan_hdd_assoc.c
+++ CORE/HDD/src/wlan_hdd_assoc.c
@@ -3426,7 +3426,7 @@ eHalStatus hdd_smeRoamCallback( void *pContext, tCsrRoamInfo *pRoamInfo, tANI_U3
                 if((pHddCtx) &&
                    (TRUE == pHddCtx->hdd_wlan_suspended) &&
                    ((eCSR_ROAM_RESULT_NONE == roamResult)||
-                     pRoamInfo->is11rAssoc))
+                     (pRoamInfo && pRoamInfo->is11rAssoc)))
                 {
                     /* Send DTIM period to the FW; only if the wlan is already
                        in suspend. This is the case with roaming (reassoc),
@@ -3454,7 +3454,7 @@ eHalStatus hdd_smeRoamCallback( void *pContext, tCsrRoamInfo *pRoamInfo, tANI_U3
                     (FULL_POWER == pmcGetPmcState(pHddCtx->hHal)) &&
                     (VOS_TRUE == pHddStaCtx->hdd_ReassocScenario) &&
                     ((eCSR_ROAM_RESULT_NONE == roamResult) ||
-                      pRoamInfo->is11rAssoc))
+                      (pRoamInfo && pRoamInfo->is11rAssoc)))
                 {
                     hddLog( LOG1, FL("Device in full power."
                            "Stop and start traffic timer for roaming"));
@@ -3468,7 +3468,7 @@ eHalStatus hdd_smeRoamCallback( void *pContext, tCsrRoamInfo *pRoamInfo, tANI_U3
 
                 halStatus = hdd_RoamSetKeyCompleteHandler( pAdapter, pRoamInfo, roamId, roamStatus, roamResult );
                 if ((eCSR_ROAM_RESULT_NONE == roamResult) ||
-                     pRoamInfo->is11rAssoc)
+                     (pRoamInfo && pRoamInfo->is11rAssoc))
                     pHddStaCtx->hdd_ReassocScenario = FALSE;
             }
             break;
