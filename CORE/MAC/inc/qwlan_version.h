2cbee6d15aa9b90348e95729a574081dcc2b9cb1
Satyanarayana Dash <c_sadash@qti.qualcomm.com>
2016-03-09 16:17:47 +0530

    Wlan: Release 3.0.10.243
    Release 3.0.10.243
    
    Change-Id: I247959e589947670e0dc0dcf156b24465ac49584
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,8 +44,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            10
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            242
+#define QWLAN_VERSION_BUILD            243
 
-#define QWLAN_VERSIONSTR               "3.0.10.242"
+#define QWLAN_VERSIONSTR               "3.0.10.243"
 
 #endif /* QWLAN_VERSION_H */
