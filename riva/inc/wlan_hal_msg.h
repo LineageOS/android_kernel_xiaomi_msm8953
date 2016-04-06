4e639dcfbe1e2e8d2f55a9b5660a14c6dcbb0539
Selvaraj, Sridhar <sselvara@qti.qualcomm.com>
2016-04-06 12:42:04 +0530

    wlan: HAL changes for dropping Actions frames in FW
    Several Action frame categories are not processed in host and they
    are not forwarded to supplicant either, but they are dropped in host.
    These unprocessed action frames are forwarded to host by FW and
    wake up host unnecessarily which leads to battery drain. With this fix,
    adding HAL interface changes for host to convey FW the allowed/required
    action frames list at HDD startup so that the remaining frames can be
    dropped in FW itself thereafter.
    
    Change-Id: Ib267bdb82b172a2de72fe166a318f218e672713d
    CRs-Fixed: 999114

--- riva/inc/wlan_hal_msg.h
+++ riva/inc/wlan_hal_msg.h
@@ -597,6 +597,8 @@ typedef enum
    WLAN_HAL_ANTENNA_DIVERSITY_SELECTION_REQ  = 330,
    WLAN_HAL_ANTENNA_DIVERSITY_SELECTION_RSP  = 331,
    WLAN_HAL_MODIFY_ROAM_PARAMS_IND           = 332,
+   WLAN_HAL_SET_ALLOWED_ACTION_FRAMES_IND    = 333,
+
    WLAN_HAL_MSG_MAX = WLAN_HAL_MSG_TYPE_MAX_ENUM_SIZE
 }tHalHostMsgType;
 
@@ -8109,6 +8111,22 @@ typedef PACKED_PRE struct PACKED_POST
    tHalMsgHeader header;
    tHalBcnMissRateRspParams bcnMissRateRspParams;
 }tHalBcnMissRateRspMsg, *tpHalBcnMissRateRspMsg;
+
+/*---------------------------------------------------------------------------
+ * WLAN_HAL_SET_ALLOWED_ACTION_FRAMES_IND
+ *-------------------------------------------------------------------------*/
+
+typedef PACKED_PRE struct PACKED_POST
+{
+   tANI_U32  actionFramesBitMask;
+   tANI_U32  reserved;
+}tHalAllowedActionFrames, *tpHalAllowedActionFrames;
+
+typedef PACKED_PRE struct PACKED_POST
+{
+   tHalMsgHeader header;
+   tHalAllowedActionFrames allowedActionFrames;
+}tHalAllowedActionFramesReqInd, *tpHalAllowedActionFramesReqInd;
 
 /*--------------------------------------------------------------------------
 * WLAN_HAL_LL_SET_STATS_REQ
