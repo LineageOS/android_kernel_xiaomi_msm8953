fa6c9f55072962b5418f0c405bca9c663a189a4c
Satyanarayana Dash <c_sadash@qti.qualcomm.com>
2016-05-12 14:50:59 +0530

    Wlan: Release 3.0.11.12
    Release 3.0.11.12
    
    Change-Id: Idc5938d06d9a0c1a3ee57ad28c55a216763742bb
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,8 +44,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            11
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            11
+#define QWLAN_VERSION_BUILD            12
 
-#define QWLAN_VERSIONSTR               "3.0.11.11"
+#define QWLAN_VERSIONSTR               "3.0.11.12"
 
 #endif /* QWLAN_VERSION_H */
