c504a22c8f2947f55bd983a4a8f851d4efbd3e66
Deepthi Gowri <c_gowri@qti.qualcomm.com>
2016-07-25 15:43:31 +0530

    prima: Fix for deadlock during tdls enable link
    During TDLS enable link, tdls lock is acquired in wlan_hdd_tdls_set_cap()
    before releasing the lock, resulting in deadlock.
    
    To address this, ensure to call the function wlan_hdd_tdls_set_cap()
    after releasing the tdls lock.
    
    CRs-Fixed: 1045653
    Change-Id: I300d29d7ef07ec3b9f1b44b3006169e19552d5f8

--- CORE/HDD/src/wlan_hdd_tdls.c
+++ CORE/HDD/src/wlan_hdd_tdls.c
@@ -1388,6 +1388,12 @@ hddTdlsPeer_t *wlan_hdd_tdls_get_peer(hdd_adapter_t *pAdapter,
     return peer;
 }
 
+/*
+ * NOTE:
+ * The Callers of this function should ensure to release the
+ * tdls_lock before calling this function to avoid deadlocks.
+ */
+
 int wlan_hdd_tdls_set_cap(hdd_adapter_t *pAdapter,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,18,0))
                                    const u8* mac,
