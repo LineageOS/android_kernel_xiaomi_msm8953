aeda6fc618e5b322e58071a6cc902b6703d07682
Rajeev Kumar Sirasanagandla <rsirasan@qti.qualcomm.com>
2016-08-17 15:01:39 +0530

    wlan: Fix overwrite of regulatory flags
    In current implementation, REGULATORY_DISABLE_BEACON_HINTS is set but
    overwritten with the REGULATORY_COUNTRY_IE_IGNORE, beacuse of which
    passive scan flags and beaconing flags are lifted by cfg80211 due to
    regulatory beacon hints.
    
    This fix this, set both flags and avoid overwrite.
    
    Change-Id: I8bc4f0c0275b9035414bdcdafa692bf1102a77cf
    CRs-Fixed: 1054854

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -7919,7 +7919,7 @@ int wlan_hdd_cfg80211_init(struct device *dev,
                  |  WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL
                     | WIPHY_FLAG_OFFCHAN_TX;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0))
-    wiphy->regulatory_flags = REGULATORY_COUNTRY_IE_IGNORE;
+    wiphy->regulatory_flags |= REGULATORY_COUNTRY_IE_IGNORE;
 #else
      wiphy->country_ie_pref = NL80211_COUNTRY_IE_IGNORE_CORE;
 #endif
