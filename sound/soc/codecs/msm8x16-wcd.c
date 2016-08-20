9a65614349a07b72356a540aaa297d80c0aa69af
flar2 <asegaert@gmail.com>
2016-08-20 12:03:51 -0400

    ASoC: msm8x16-wcd: Sound control: allow further reduction of digital gain
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -5780,10 +5780,10 @@ static ssize_t headphone_gain_store(struct kobject *kobj,
 
 	sscanf(buf, "%d %d", &input_l, &input_r);
 
-	if (input_l < -10 || input_l > 20)
+	if (input_l < -84 || input_l > 20)
 		input_l = 0;
 
-	if (input_r < -10 || input_r > 20)
+	if (input_r < -84 || input_r > 20)
 		input_r = 0;
 
 	snd_soc_write(sound_control_codec_ptr, MSM8X16_WCD_A_CDC_RX1_VOL_CTL_B2_CTL, input_l);
