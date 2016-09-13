1ab8b6604c372a46a5b7fbbb1807f7bd64cc8be4
Satyanarayana Dash <c_sadash@qti.qualcomm.com>
2016-09-13 15:13:05 +0530

    Wlan: Release 3.0.11.56
    Release 3.0.11.56
    
    Change-Id: I3622ded4fef16166f6a3814eb0cd00fbbb840a7a
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,8 +44,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            11
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            55
+#define QWLAN_VERSION_BUILD            56
 
-#define QWLAN_VERSIONSTR               "3.0.11.55"
+#define QWLAN_VERSIONSTR               "3.0.11.56"
 
 #endif /* QWLAN_VERSION_H */
