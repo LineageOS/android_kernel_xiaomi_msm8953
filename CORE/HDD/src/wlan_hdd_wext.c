893801aa1f1a9c0cee264441242f475f24234eb0
Sravan Kumar Kairam <sgoud@qti.qualcomm.com>
2016-11-01 19:14:23 +0530

    wlan: Reset Rx last SSN after clear filter
    Currently, when MSDU reorder info exists for a tid of station, then
    after clear filter host last Rx packet sequence number SSN goes out
    of sync with the hardware. Reason is in the period between set and
    clear filter packets coming to device will be dropped at the firmware
    because of the filter applied and host still has the last SSN before
    set filter. Also RX packet's SSN will wrap around and start from zero
    if SSN crosses 4096 in the period between set and clear filters.
    
    After clear filter because of above reasons RX packet SSN in issue case
    will be less than the host last RX SSN and when the difference is more
    than threshold which in this case is 3000 packet gets dropped.
    
    In this change reset the last Rx SSN to zero for the station's tid
    MSDU reorder info if exists.
    
    Change-Id: I30fb1a27efea2e57a229f674e0c2d80805847de5
    CRs-Fixed: 1084362

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -8977,7 +8977,6 @@ static void hdd_pkt_filter_done(void *data, v_U32_t status)
 {
    struct statsContext *cbCtx = (struct statsContext *)data;
 
-
    hddLog(VOS_TRACE_LEVEL_INFO,
               FL("Pkt Filter Clear Status : %d"), status);
 
@@ -9007,6 +9006,7 @@ static void hdd_pkt_filter_done(void *data, v_U32_t status)
 int wlan_hdd_set_filter(hdd_adapter_t *pAdapter, tpPacketFilterCfg pRequest)
 {
     hdd_context_t *pHddCtx = WLAN_HDD_GET_CTX(pAdapter);
+    hdd_station_ctx_t *pHddStaCtx = &pAdapter->sessionCtx.station;
     tSirRcvPktFilterCfgType    packetFilterSetReq = {0};
     tSirRcvFltPktClearParam    packetFilterClrReq = {0};
     struct statsContext        cbCtx;
@@ -9095,6 +9095,12 @@ int wlan_hdd_set_filter(hdd_adapter_t *pAdapter, tpPacketFilterCfg pRequest)
 
             hddLog(VOS_TRACE_LEVEL_INFO_HIGH, "%s: Clear Packet Filter Request for Id: %d",
                     __func__, pRequest->filterId);
+
+            if ((WLAN_HDD_INFRA_STATION == pAdapter->device_mode) ||
+                (WLAN_HDD_P2P_CLIENT == pAdapter->device_mode)) {
+               WLANTL_ResetRxSSN((WLAN_HDD_GET_CTX(pAdapter))->pvosContext,
+                                 pHddStaCtx->conn_info.staId[0]);
+            }
 
             init_completion(&cbCtx.completion);
             cbCtx.magic = CLEAR_FILTER_MAGIC;
