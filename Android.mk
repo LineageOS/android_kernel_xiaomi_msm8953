e49a3662b2564c9542a839a63dea6fad6d0febdc
Anjaneedevi Kapparapu <akappa@codeaurora.org>
2016-11-03 17:26:15 +0530

    resolving Fast-forward to 'jb_mr1' to wlan-driver.lnx.1.0-dev.1.0 issues
    added the diff code inbetween jb_mr1 and wlan-driver.lnx.1.0-dev.1.0
    fastforwd Idd8c02dd669aca173ac243e232ab2189c690ff70
    
    Change-Id: If3d28705014d95c420a81a2641b123d283fa1802
    CRs-Fixed: 1084347

--- Android.mk
+++ Android.mk
@@ -10,7 +10,7 @@ WLAN_SELECT := CONFIG_PRIMA_WLAN=m
 endif
 
 # Build/Package options for 8916, 8974, 8226, 8610, 8909, 8952, 8937, 8953 targets
-ifneq (,$(filter msm8916 msm8974 msm8226 msm8610 msm8909 msm8952 msm8937 msm8953 msm8953,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter msm8916 msm8974 msm8226 msm8610 msm8909 msm8952 msm8937 msm8953 titanium,$(TARGET_BOARD_PLATFORM)))
 WLAN_CHIPSET := pronto
 WLAN_SELECT := CONFIG_PRONTO_WLAN=m
 endif
