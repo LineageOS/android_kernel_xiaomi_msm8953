c13f5e277fc53628da5467e96a6c2cc5a8ec468d
AnjaneeDevi Kapparapu <c_akappa@qti.qualcomm.com>
2016-12-13 16:00:24 +0530

    Wlan: Release 3.0.11.82
    Release 3.0.11.82
    
    Change-Id: I2c5390a260ada89fdd884d7607e1de29dca5b0d8
    CRs-Fixed: 688141

--- CORE/MAC/inc/qwlan_version.h
+++ CORE/MAC/inc/qwlan_version.h
@@ -44,8 +44,8 @@ BRIEF DESCRIPTION:
 #define QWLAN_VERSION_PATCH            11
 #define QWLAN_VERSION_EXTRA            ""
 
-#define QWLAN_VERSION_BUILD            81
+#define QWLAN_VERSION_BUILD            82
 
-#define QWLAN_VERSIONSTR               "3.0.11.81"
+#define QWLAN_VERSIONSTR               "3.0.11.82"
 
 #endif /* QWLAN_VERSION_H */
