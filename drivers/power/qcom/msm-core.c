db9185bb08885ff301ac9a5d03b0a4212f215307
Alex Naidis <alex.naidis@linux.com>
2017-05-01 14:16:05 +0200

    msm-core: Fix error handling of sensor data retrieval
    trigger_cpu_pwr_stats_calc was ignoring the return code
    of sensor_get_temp.
    
    We need to handle the return code for the error case
    and report a default temperature instead.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    
     Conflicts:
    	drivers/power/qcom/msm-core.c
    
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -234,7 +234,7 @@ static void repopulate_stats(int cpu)
 void trigger_cpu_pwr_stats_calc(void)
 {
 #ifdef ENABLE_TSENS_SAMPLING
-	int cpu;
+	int cpu, rc;
 	static long prev_temp[NR_CPUS];
 	struct cpu_activity_info *cpu_node;
 	long temp;
@@ -250,9 +250,14 @@ void trigger_cpu_pwr_stats_calc(void)
 			continue;
 
 		if (cpu_node->temp == prev_temp[cpu]) {
-			sensor_get_temp(cpu_node->sensor_id, &temp);
-			cpu_node->temp = temp / scaling_factor;
+			rc = sensor_get_temp(cpu_node->sensor_id, &temp);
+			if (rc) {
+				pr_err("msm-core: The sensor reported invalid data!");
+				temp = DEFAULT_TEMP;
+			}
 		}
+
+		cpu_node->temp = temp / scaling_factor;
 
 		prev_temp[cpu] = cpu_node->temp;
 
