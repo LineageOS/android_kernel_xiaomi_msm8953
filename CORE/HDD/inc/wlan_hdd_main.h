5addea4d8ca20f2831942e0134bbec2123be0ca7
Hanumanth Reddy Pothula <c_hpothu@codeaurora.org>
2017-05-19 13:56:35 +0530

    wlan: Send fatal event logging request to FW only if it is supported
    Presently, host sending fatal event logging request to firmware
    without checking whether the same is supported in firmware, leading
    fatal event request sent by the host to timed-out.
    
    Send fatal event logging request to firmware only if firmware supports
    the same.
    
    Change-Id: I68f74af539219bb6f5e95b5df839faf62205a03b
    CRs-Fixed: 2049113

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -1689,7 +1689,7 @@ struct hdd_context_s
     scan_reject_states last_scan_reject_reason;
     v_TIME_t last_scan_reject_timestamp;
     v_U8_t scan_reject_cnt;
-
+    bool is_fatal_event_log_sup;
     uint32_t track_arp_ip;
 };
 
