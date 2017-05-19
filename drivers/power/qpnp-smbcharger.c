ff7a78b5bc8d0c6322e2e06a03a38ec1bb055dd6
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-05-19 16:44:44 +0800

    drivers: input: touchscreen: Add ft5346 and ist3038c drivers
    * touchscreens used on Xiaomi Redmi Note 4 (mido)
    * unused firmwares dropped for both
    * test code for ft5346 droped
    * added code to qpnp-smbcharger to notify touchscreen about charger
    connection

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -4721,12 +4721,18 @@ static int smbchg_restricted_charging(struct smbchg_chip *chip, bool enable)
 
 	return rc;
 }
-
+extern void ist30xx_set_ta_mode(bool mode);
+extern void tpd_usb_plugin(bool mode);
+int set_usb_charge_mode_par = 0;
 static void handle_usb_removal(struct smbchg_chip *chip)
 {
 	struct power_supply *parallel_psy = get_parallel_psy(chip);
 	int rc;
-
+	if (set_usb_charge_mode_par == 1) {
+		ist30xx_set_ta_mode(0);
+	} else if (set_usb_charge_mode_par == 2) {
+		tpd_usb_plugin(0);
+	}
 	pr_smb(PR_STATUS, "triggered\n");
 	smbchg_aicl_deglitch_wa_check(chip);
 	/* Clear the OV detected status set before */
@@ -4795,7 +4801,11 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	enum power_supply_type usb_supply_type;
 	int rc;
 	char *usb_type_name = "null";
-
+	if (set_usb_charge_mode_par == 1) {
+		ist30xx_set_ta_mode(1);
+	} else if (set_usb_charge_mode_par == 2) {
+		tpd_usb_plugin(1);
+	}
 	pr_smb(PR_STATUS, "triggered\n");
 	/* usb inserted */
 	read_usb_type(chip, &usb_type_name, &usb_supply_type);
