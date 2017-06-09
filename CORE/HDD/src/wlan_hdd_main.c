e21a521907d332e33d2b9f30743fb4ba5d2c904a
Abhishek Singh <absingh@codeaurora.org>
2017-06-09 09:42:58 +0530

    wlan: Indicate SME of disconnect during stop adapter
    Indicate SME of disconnect during stop adapter so that it can stop
    trying for next BSSID to connect and thus avoid disconnect timeout.
    
    Change-Id: I3e243e67b115bb399ff7a26ea1b50e599b8d365c
    CRs-Fixed: 2058751

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -9022,6 +9022,12 @@ VOS_STATUS hdd_stop_adapter( hdd_context_t *pHddCtx, hdd_adapter_t *pAdapter,
          if( hdd_connIsConnected(pstation) ||
              (pstation->conn_info.connState == eConnectionState_Connecting) )
          {
+            /*
+             * Indicate sme of disconnect so that in progress connection
+             * or preauth can be aborted.
+             */
+            sme_abortConnection(WLAN_HDD_GET_HAL_CTX(pAdapter),
+                            pAdapter->sessionId);
             INIT_COMPLETION(pAdapter->disconnect_comp_var);
             if (pWextState->roamProfile.BSSType == eCSR_BSS_TYPE_START_IBSS)
                 halStatus = sme_RoamDisconnect(pHddCtx->hHal,
