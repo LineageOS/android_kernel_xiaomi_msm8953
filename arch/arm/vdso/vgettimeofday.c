ed0af6f3c0cf6c1bb263e8afd6785f1584e25573
Mark Salyzyn <salyzyn@google.com>
2017-08-17 09:16:27 -0700

    FROMLIST: [PATCH v5 08/12] arm: vdso: Add ARCH_CLOCK_FIXED_MASK
    (cherry picked from url https://patchwork.kernel.org/patch/10044543/)
    
    Take an effort to recode the arm64 vdso code from assembler to C
    previously submitted by Andrew Pinski <apinski@cavium.com>, rework
    it for use in both arm and arm64, overlapping any optimizations
    for each architecture. But instead of landing it in arm64, land the
    result into lib/vdso and unify both implementations to simplify
    future maintenance.
    
    Add ARCH_CLOCK_FIXED_MASK as an efficiency since arm64 has no
    purpose for cs_mask vdso_data variable.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Andy Lutomirski <luto@amacapital.net>
    Cc: Dmitry Safonov <dsafonov@virtuozzo.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Andy Gross <andy.gross@linaro.org>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Andrew Pinski <apinski@cavium.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-arm-kernel@lists.infradead.org
    Bug: 63737556
    Bug: 20045882
    Change-Id: Iadf94bed6166d2ee43bb46bdf54636618e4b8854

--- arch/arm/vdso/vgettimeofday.c
+++ arch/arm/vdso/vgettimeofday.c
@@ -131,7 +131,11 @@ static notrace int do_realtime(const struct vdso_data *vd, struct timespec *ts)
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
+#ifdef ARCH_CLOCK_FIXED_MASK
+	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
+#else
 	u64 mask;
+#endif
 	vdso_xtime_clock_sec_t sec;
 
 	do {
@@ -144,7 +148,9 @@ static notrace int do_realtime(const struct vdso_data *vd, struct timespec *ts)
 
 		mult = vd->cs_mono_mult;
 		shift = vd->cs_shift;
+#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
+#endif
 
 		sec = vd->xtime_clock_sec;
 		nsec = vd->xtime_clock_snsec;
@@ -164,7 +170,11 @@ static notrace int do_monotonic(const struct vdso_data *vd, struct timespec *ts)
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
+#ifdef ARCH_CLOCK_FIXED_MASK
+	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
+#else
 	u64 mask;
+#endif
 	vdso_wtm_clock_nsec_t wtm_nsec;
 	__kernel_time_t sec;
 
@@ -178,7 +188,9 @@ static notrace int do_monotonic(const struct vdso_data *vd, struct timespec *ts)
 
 		mult = vd->cs_mono_mult;
 		shift = vd->cs_shift;
+#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
+#endif
 
 		sec = vd->xtime_clock_sec;
 		nsec = vd->xtime_clock_snsec;
@@ -203,7 +215,11 @@ static notrace int do_monotonic_raw(const struct vdso_data *vd,
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
+#ifdef ARCH_CLOCK_FIXED_MASK
+	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
+#else
 	u64 mask;
+#endif
 	vdso_raw_time_sec_t sec;
 
 	do {
@@ -216,7 +232,9 @@ static notrace int do_monotonic_raw(const struct vdso_data *vd,
 
 		mult = vd->cs_raw_mult;
 		shift = vd->cs_shift;
+#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
+#endif
 
 		sec = vd->raw_time_sec;
 		nsec = vd->raw_time_nsec;
