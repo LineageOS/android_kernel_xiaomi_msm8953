920902075ae86a200167cb1ed78863f0136e6c57
Rajeev Kumar Sirasanagandla <rsirasan@codeaurora.org>
2017-08-23 18:07:01 +0530

    wlan: Check for space termination of driver command name
    qcacld-2.0 to prima propagation.
    
    Currently, few hdd driver commands and hostapd driver commands which
    expect input are not checked for space termination of command name.
    Skip of this check can result in buffer overread error.
    
    To fix this, add check based on the length of command name.
    
    Change-Id: I92a4efd7e729a9d792af166b4a09963886c9942f
    CRs-Fixed: 2096872

--- CORE/HDD/inc/wlan_hdd_main.h
+++ CORE/HDD/inc/wlan_hdd_main.h
@@ -2047,4 +2047,16 @@ int hdd_reassoc(hdd_adapter_t *pAdapter, const tANI_U8 *bssid,
 
 void wlan_hdd_start_sap(hdd_adapter_t *ap_adapter);
 
+/**
+ * hdd_drv_cmd_validate() - Validates for space in hdd driver command
+ * @command: pointer to input data (its a NULL terminated string)
+ * @len: length of command name
+ *
+ * This function checks for space after command name and if no space
+ * is found returns error.
+ *
+ * Return: 0 for success non-zero for failure
+ */
+int hdd_drv_cmd_validate(tANI_U8 *command, int len);
+
 #endif    // end #if !defined( WLAN_HDD_MAIN_H )
