a39616162380d01f98d399efb008e8da3214eb13
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2018-01-04 14:24:50 +0530

    wlan: Remove notify to lim flag for del BA sessions cfg
    Del BA sessions cfg is set to enabled on receiving disable
    aggregation indication during SCO which will post message
    to lim as part of notify to delete BA sessions. If aggregation
    during SCO feature is enabled, del BA sessions cfg is reset to
    disabled. The message posted to lim is processed based on del
    BA cfg value and since del ba sessions cfg is reset to disabled,
    BA sessions are not deleted.
    
    Fix is to remove notify flag for del ba sessions cfg and
    post message to lim to delete ba sessions on receiving the
    disable aggregation indication.
    
    Change-Id: I4b6ceb86dc3368d5f93ec26250034599b53f7317
    CRs-Fixed: 2166984

--- CORE/MAC/src/pe/lim/limUtils.h
+++ CORE/MAC/src/pe/lim/limUtils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -381,7 +381,6 @@ void limDeleteStaContext(tpAniSirGlobal pMac, tpSirMsgQ limMsg);
 void limProcessAddBaInd(tpAniSirGlobal pMac, tpSirMsgQ limMsg);
 void limDeleteBASessions(tpAniSirGlobal pMac, tpPESession pSessionEntry,
                          tANI_U32 baDirection, tSirMacReasonCodes baReasonCode);
-void limDelPerBssBASessionsBtc(tpAniSirGlobal pMac);
 void limDelAllBASessions(tpAniSirGlobal pMac);
 void limDeleteDialogueTokenList(tpAniSirGlobal pMac);
 tSirRetStatus limSearchAndDeleteDialogueToken(tpAniSirGlobal pMac, tANI_U8 token, tANI_U16 assocId, tANI_U16 tid);
