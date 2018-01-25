5c44a3ae2bfc9b08b276d9ddf2915ab905bb201b
TheScarastic <warabhishek@gmail.com>
2018-01-25 21:21:14 +0530

    drivers: input: touchscreen: gt9xx_mido: Create shared procfs nodes
    * Creates sysfs nodes and procfs symlinks to it to easy handle
    disable_keys and dt2w functions from userspace
    
    [flex1911: Update kernfs_path usage for 4.9]

--- drivers/input/touchscreen/gt9xx_mido/gt9xx.c
+++ drivers/input/touchscreen/gt9xx_mido/gt9xx.c
@@ -56,6 +56,7 @@ extern int set_usb_charge_mode_par;
 
 int gt9xx_id;
 int gt9xx_flag;
+static bool disable_keys_function = false;
 
 
 
@@ -876,7 +877,7 @@ static void goodix_ts_work_func(struct work_struct *work)
 	#endif
 
 	#if GTP_HAVE_TOUCH_KEY
-		if (!pre_touch) {
+		if (!pre_touch && !disable_keys_function) {
 			for (i = 0; i < GTP_MAX_KEY_NUM; i++) {
 			#if GTP_DEBUG_ON
 				for (ret = 0; ret < 4; ++ret) {
@@ -2558,6 +2559,107 @@ void gtp_usb_plugin(bool mode)
 	GTP_ERROR("GTP send Charge cmd failed.");
 }
 
+static ssize_t gt9xx_mido_disable_keys_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	const char c = disable_keys_function ? '1' : '0';
+	return sprintf(buf, "%c\n", c);
+}
+
+static ssize_t gt9xx_mido_disable_keys_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	int i;
+
+	if (sscanf(buf, "%u", &i) == 1 && i < 2) {
+		disable_keys_function = (i == 1);
+		return count;
+	} else {
+		dev_dbg(dev, "disable_keys write error\n");
+		return -EINVAL;
+	}
+}
+
+static DEVICE_ATTR(disable_keys, S_IWUSR | S_IRUSR, gt9xx_mido_disable_keys_show,
+		   gt9xx_mido_disable_keys_store);
+
+static ssize_t gt9xx_mido_enable_dt2w_show(struct device *dev,
+        struct device_attribute *attr, char *buf)
+{
+        const char c = gtp_gesture_func_on  ? '1' : '0';
+        return sprintf(buf, "%c\n", c);
+}
+
+static ssize_t gt9xx_mido_enable_dt2w_store(struct device *dev,
+        struct device_attribute *attr, const char *buf, size_t count)
+{
+        int i;
+
+        if (sscanf(buf, "%u", &i) == 1 && i < 2) {
+                gtp_gesture_func_on  = (i == 1);
+                return count;
+        } else {
+                dev_dbg(dev, "enable_dt2w write error\n");
+                return -EINVAL;
+        }
+}
+
+
+static DEVICE_ATTR(enable_dt2w, S_IWUSR | S_IRUSR, gt9xx_mido_enable_dt2w_show,
+                   gt9xx_mido_enable_dt2w_store);
+
+static struct attribute *gt9xx_mido_attrs[] = {
+    &dev_attr_disable_keys.attr,
+    &dev_attr_enable_dt2w.attr,
+	NULL
+};
+
+static const struct attribute_group gt9xx_mido_attr_group = {
+       .attrs = gt9xx_mido_attrs,
+};
+
+static int gt9xx_mido_proc_init(struct kernfs_node *sysfs_node_parent)
+{
+       int ret = 0;
+       char *buf;
+       char *key_disabler_sysfs_node, *double_tap_sysfs_node;
+       struct proc_dir_entry *proc_entry_tp = NULL;
+       struct proc_dir_entry *proc_symlink_tmp = NULL;
+       buf = kzalloc(PATH_MAX, GFP_KERNEL);
+       if (buf)
+               kernfs_path(sysfs_node_parent, buf, PATH_MAX);
+
+       proc_entry_tp = proc_mkdir("touchpanel", NULL);
+       if (proc_entry_tp == NULL) {
+               pr_err("%s: Couldn't create touchpanel dir in procfs\n", __func__);
+               ret = -ENOMEM;
+       }
+
+       key_disabler_sysfs_node = kzalloc(PATH_MAX, GFP_KERNEL);
+       if (key_disabler_sysfs_node)
+               sprintf(key_disabler_sysfs_node, "/sys%s/%s", buf, "disable_keys");
+       proc_symlink_tmp = proc_symlink("capacitive_keys_disable",
+                       proc_entry_tp, key_disabler_sysfs_node);
+       if (proc_symlink_tmp == NULL) {
+               pr_err("%s: Couldn't create capacitive_keys_enable symlink\n", __func__);
+               ret = -ENOMEM;
+       }
+
+       double_tap_sysfs_node = kzalloc(PATH_MAX, GFP_KERNEL);
+       if (double_tap_sysfs_node)
+               sprintf(double_tap_sysfs_node, "/sys%s/%s", buf, "enable_dt2w");
+       proc_symlink_tmp = proc_symlink("enable_dt2w",
+               proc_entry_tp, double_tap_sysfs_node);
+       if (proc_symlink_tmp == NULL) {
+               ret = -ENOMEM;
+               pr_err("%s: Couldn't create double_tap_enable symlink\n", __func__);
+       }
+
+       kfree(buf);
+       kfree(key_disabler_sysfs_node);
+       kfree(double_tap_sysfs_node);
+       return ret;
+}
 
 /*******************************************************
 Function:
@@ -2713,6 +2815,14 @@ static int goodix_ts_probe(struct i2c_client *client, const struct i2c_device_id
 	} else {
 		GTP_INFO("create_glove_proc %s success", GT91XX_Glove_PROC_FILE);
 	}
+
+        ret = sysfs_create_group(&client->dev.kobj, &gt9xx_mido_attr_group);
+        if (ret) {
+	        dev_err(&client->dev, "Failure %d creating sysfs group\n",
+		        ret);
+                sysfs_remove_group(&client->dev.kobj, &gt9xx_mido_attr_group);
+        }
+	gt9xx_mido_proc_init(client->dev.kobj.sd);
 
 
 #if GTP_ESD_PROTECT
