115f2cf7fa54f234b69ee43cbfe919deec1a4136
Soumya Managoli <smanag@codeaurora.org>
2018-02-26 18:42:53 +0530

    ASoC: config: Enable CPE for sdm450 external audio codec
    Enable compilation of msm-cpe-lsm module.
    
    CRs-Fixed: 2165776
    Change-Id: If8956572e9bb718d0996c72027f03373578f3b46
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>

--- asoc/Android.mk
+++ asoc/Android.mk
@@ -56,7 +56,7 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 ###########################################################
-ifeq ($(call is-board-platform-in-list,sdm670 qcs605),true)
+ifeq ($(call is-board-platform-in-list,msm8953 sdm670 qcs605),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE              := $(AUDIO_CHIPSET)_cpe_lsm.ko
 LOCAL_MODULE_KBUILD_NAME  := cpe_lsm_dlkm.ko
