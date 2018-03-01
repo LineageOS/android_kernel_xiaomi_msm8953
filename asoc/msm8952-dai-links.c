d4db381b14246608e2434f6222255bee11ef7cca
Soumya Managoli <smanag@codeaurora.org>
2018-03-01 17:40:05 +0530

    ASoC: Update platform name for CPE dai link
    Change to update with appropriate platform
    name for CPE Listen service dai on sdm450
    external codec driver.
    
    CRs-Fixed: 2165776
    Change-Id: I979b8e5717a961aac0ed1a9abb708032e88c1493
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>

--- asoc/msm8952-dai-links.c
+++ asoc/msm8952-dai-links.c
@@ -119,7 +119,7 @@ static struct snd_soc_dai_link msm8952_tasha_fe_dai[] = {
 		.name = "CPE Listen service",
 		.stream_name = "CPE Listen Audio Service",
 		.cpu_dai_name = "msm-dai-slim",
-		.platform_name = "msm-pcm-hostless",
+		.platform_name = "msm-cpe-lsm",
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
 			SND_SOC_DPCM_TRIGGER_POST},
 		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
