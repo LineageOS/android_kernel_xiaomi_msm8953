1d5d4cc46246eb5fdaeaedfb4b485257e7c2c814
TheScarastic <warabhishek@gmail.com>
2018-03-19 16:53:17 +0530

    fs: pstore: Import xiaomi pstore changes
    Change-Id: I6d02f223aa310583caa0773f1b7ed8e3f4a06cc7

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -262,11 +262,25 @@ ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 	return ret;
 }
 
+static void *memcpy_pstore(void *dest, const void *src, size_t count)
+{
+	char *tmp = dest;
+	const char *s = src;
+
+	while (count--)
+		*tmp++ = *s++;
+	return dest;
+}
+
 static void notrace persistent_ram_update(struct persistent_ram_zone *prz,
 	const void *s, unsigned int start, unsigned int count)
 {
 	struct persistent_ram_buffer *buffer = prz->buffer;
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
+	memcpy_pstore(buffer->data + start, s, count);
+#else
 	memcpy_toio(buffer->data + start, s, count);
+#endif
 	persistent_ram_update_ecc(prz, start, count);
 }
 
