dc5f6450fef3467b9a7b845293c923895120c2c4
TheScarastic <warabhishek@gmail.com>
2018-03-23 13:49:02 +0530

    kernel: Add Xiaomi 5X support
    drivers: platform: Add Xiaomi 5X support
    
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: Ic87464687af6777e7ee57099d1080c3a469f1cf2
    
    fs: pstore: Add Xiaomi 5X support
    
    Change-Id: Iba47e63fe5990ac380278b16c14ca497dc09fac5
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    
    driver: video: mdss: Add Xiaomi 5X support
    
    Change-Id: Ic674b6db426bab33c98fb052b1c0b530601d9f3b
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    
    drivers: power: Add Xiaomi 5X support
    
    Change-Id: I3695ca28d6794fcfb409ac98898fe87dfd1202ca
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    
    drivers: media: camera_v2: Add Xiaomi 5X support
    
    Change-Id: I92524137d53003a7374eb525cc158777fa276f70
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    
    techpack: Import initial changes for Xiaomi msm8953
    
    * Adapted for 4.9 drivers
    * Unused stuff, such as the h2w switch, was omitted
    
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: Iabb440224cc841b55e5ada2406b323475ed7ff4a

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -6074,7 +6074,7 @@ static int smbchg_battery_get_property(struct power_supply *psy,
 		val->intval = get_prop_batt_health(chip);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-#ifdef CONFIG_MACH_XIAOMI_TISSOT
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TIFFANY) || (defined CONFIG_MACH_XIAOMI_TISSOT)
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
 #else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
@@ -6291,7 +6291,7 @@ static irqreturn_t batt_warm_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
-#ifdef CONFIG_MACH_XIAOMI_TISSOT
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TIFFANY) || (defined CONFIG_MACH_XIAOMI_TISSOT)
 	int rc;
 	/* set the warm float voltage compensation,set the warm float voltage to 4.1V */
 	if (chip->float_voltage_comp != -EINVAL) {
@@ -6319,7 +6319,7 @@ static irqreturn_t batt_cool_handler(int irq, void *_chip)
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
 
-#ifdef CONFIG_MACH_XIAOMI_TISSOT
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TIFFANY) || (defined CONFIG_MACH_XIAOMI_TISSOT)
 	int rc;
 	/* set the cool float voltage compensation ,set the cool float voltage to 4.4V*/
 	rc = smbchg_float_voltage_comp_set(chip, 0);
@@ -8423,7 +8423,7 @@ static int smbchg_probe(struct platform_device *pdev)
 		goto votables_cleanup;
 	}
 
-#ifdef CONFIG_MACH_XIAOMI_TISSOT
+#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TIFFANY) || (defined CONFIG_MACH_XIAOMI_TISSOT)
 	chip->hvdcp_not_supported = true;
 #endif
 
