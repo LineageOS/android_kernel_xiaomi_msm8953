a61e0a55bd3f0ddb06662d805f7688106300d9a5
TheScarastic <warabhishek@gmail.com>
2018-03-23 19:06:06 +0530

    drivers: power: Import tissot changes
    Change-Id: I0065527aeb03c349d1e375373ef40f2ce7fb5bd2

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -320,10 +320,16 @@ struct device_node *of_batterydata_get_best_profile(
 {
 	struct batt_ids batt_ids;
 	struct device_node *node, *best_node = NULL;
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+	struct device_node *default_node = NULL;
+#endif
 	const char *battery_type = NULL;
 	int delta = 0, best_delta = 0, best_id_kohm = 0, id_range_pct,
 		i = 0, rc = 0, limit = 0;
 	bool in_range = false;
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+	int checknum = 0, match = 0;
+#endif
 
 	/* read battery id range percentage for best profile */
 	rc = of_property_read_u32(batterydata_container_node,
@@ -360,11 +366,19 @@ struct device_node *of_batterydata_get_best_profile(
 				delta = abs(batt_ids.kohm[i] - batt_id_kohm);
 				limit = (batt_ids.kohm[i] * id_range_pct) / 100;
 				in_range = (delta <= limit);
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+				if (in_range != 0)
+					match = 1;
+#endif
 				/*
 				 * Check if the delta is the lowest one
 				 * and also if the limits are in range
 				 * before selecting the best node.
 				 */
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+				if (batt_ids.kohm[i] == 50)
+					default_node = node;
+#endif
 				if ((delta < best_delta || !best_node)
 					&& in_range) {
 					best_node = node;
@@ -375,13 +389,24 @@ struct device_node *of_batterydata_get_best_profile(
 		}
 	}
 
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+	checknum = abs(best_id_kohm - batt_id_kohm);
+	if (match == 0) {
+		best_node = default_node;
+		checknum = 0;
+	}
+#endif
 	if (best_node == NULL) {
 		pr_err("No battery data found\n");
 		return best_node;
 	}
 
 	/* check that profile id is in range of the measured batt_id */
+#ifdef CONFIG_MACH_XIAOMI_TISSOT
+	if (checknum >
+#else
 	if (abs(best_id_kohm - batt_id_kohm) >
+#endif
 			((best_id_kohm * id_range_pct) / 100)) {
 		pr_err("out of range: profile id %d batt id %d pct %d",
 			best_id_kohm, batt_id_kohm, id_range_pct);
