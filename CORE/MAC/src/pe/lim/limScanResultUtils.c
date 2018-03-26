c3bfe0aa7c9a57c7bc2250a1bef6d3f9cc11c92b
Wu Gao <wugao@codeaurora.org>
2018-03-26 13:11:28 +0800

    prima: Add BSS if new channel same to current channel in ECSA IE
    In some special case, reference AP or P2P GO included ECSA IE wrongly,
    and the new channel is same to current channel.
    
    Change-Id: Iae6b55db913a47bf6199ac0d490d1c10017c471b
    CRs-Fixed: 2209738

--- CORE/MAC/src/pe/lim/limScanResultUtils.c
+++ CORE/MAC/src/pe/lim/limScanResultUtils.c
@@ -450,9 +450,23 @@ limCheckAndAddBssDescription(tpAniSirGlobal pMac,
      * caching the scan results for APs which are adverzing the channel-switch
      * element in their beacons and probe responses.
      */
-    if(pBPR->channelSwitchPresent || pBPR->ecsa_present)
+    if(pBPR->channelSwitchPresent)
     {
         return;
+    }
+
+    if(pBPR->ecsa_present) {
+       limLog(pMac, LOGW, FL("ECSA IE present"));
+       /* Still add to scan result if ECSA IE present and new channel
+        * equal to current channel.
+        */
+       if (pBPR->channelNumber!= HAL_INVALID_CHANNEL_ID &&
+           pBPR->ext_chan_switch_ann.new_channel != HAL_INVALID_CHANNEL_ID &&
+           pBPR->channelNumber != pBPR->ext_chan_switch_ann.new_channel) {
+             limLog(pMac, LOGW, FL("ignore this AP"));
+             return;
+        }
+
     }
 
     /* If beacon/probe resp DS param channel does not match with 
