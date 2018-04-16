3ad15d5bb7af0b4d4612ff6b8413d6b3602b4340
Yeshwanth Sriram Guntuka <ysriramg@codeaurora.org>
2018-04-16 14:34:25 +0530

    wlan: Drop probe resp only if ecsa new chn is different from APs oper chn
    IOT AP is advertising ecsa with new channel same as the current
    operating channel in probe response. DUT is dropping these frames
    based on ecsa present check. This results in connection latency
    as bss info of this AP is not informed to supplicant.
    
    Fix is to drop probe response having ecsa element only if new
    channel is different from APs current operating channel.
    
    Change-Id: Ia42a889b71af14f628cf532b6944322d049f04d5
    CRs-Fixed: 2217394

--- CORE/MAC/src/pe/lim/limScanResultUtils.c
+++ CORE/MAC/src/pe/lim/limScanResultUtils.c
@@ -452,7 +452,9 @@ limCheckAndAddBssDescription(tpAniSirGlobal pMac,
      */
     if(pBPR->channelSwitchPresent)
     {
-        return;
+       if (pBPR->ext_chan_switch_ann.new_channel !=
+           limGetChannelFromBeacon(pMac, pBPR))
+           return;
     }
 
     if(pBPR->ecsa_present) {
