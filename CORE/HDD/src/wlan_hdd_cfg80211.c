f852cc5647db2d3f56aff9e4c52679f285b7b74c
Dundi Raviteja <dundi@codeaurora.org>
2018-09-18 09:53:09 +0530

    prima: Wi-Fi direct is not working when SAP is turned on
    Currently while changing interface, if interface type is AP
    and p2p adapter present, driver removes p2p interface which
    cause Wi-Fi direct to disable.
    
    To address this issue, don't remove p2p interface while changing
    interface.
    
    Change-Id: I13c7d5230a65d2fb66099de52b79f19ec4072135
    CRs-Fixed: 2314145

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -12294,7 +12294,6 @@ int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
     struct wireless_dev *wdev;
     hdd_adapter_t *pAdapter = WLAN_HDD_GET_PRIV_PTR( ndev );
     hdd_context_t *pHddCtx;
-    hdd_adapter_t  *pP2pAdapter = NULL;
     tCsrRoamProfile *pRoamProfile = NULL;
     eCsrRoamBssType LastBSSType;
     hdd_config_t *pConfig = NULL;
@@ -12459,25 +12458,6 @@ int __wlan_hdd_cfg80211_change_iface( struct wiphy *wiphy,
                 if (NL80211_IFTYPE_P2P_GO == type)
                 {
                     wlan_hdd_cancel_existing_remain_on_channel(pAdapter);
-                }
-                if (NL80211_IFTYPE_AP == type)
-                {
-                     /* As Loading WLAN Driver one interface being created for p2p device
-                      * address. This will take one HW STA and the max number of clients
-                      * that can connect to softAP will be reduced by one. so while changing
-                      * the interface type to NL80211_IFTYPE_AP (SoftAP) remove p2p0
-                      * interface as it is not required in SoftAP mode.
-                      */
-
-                    // Get P2P Adapter
-                    pP2pAdapter = hdd_get_adapter(pHddCtx, WLAN_HDD_P2P_DEVICE);
-
-                    if (pP2pAdapter)
-                    {
-                        hdd_stop_adapter(pHddCtx, pP2pAdapter, VOS_TRUE);
-                        hdd_deinit_adapter(pHddCtx, pP2pAdapter, TRUE);
-                        hdd_close_adapter(pHddCtx, pP2pAdapter, VOS_TRUE);
-                    }
                 }
                 //Disable IMPS & BMPS for SAP/GO
                 if(VOS_STATUS_E_FAILURE ==
