5b222127f868770590b3c2da5de968d65b6b3acd
Artem Borisov <dedsa2002@gmail.com>
2018-10-29 13:41:04 +0400

    techpack: Fix the module init sequence
    Fixes panics when built-in.
    
    Change-Id: I095d00d9623d16b413533749bcc1d31df63a4e1c

--- techpack/audio/dsp/q6_init.c
+++ techpack/audio/dsp/q6_init.c
@@ -19,7 +19,6 @@ GNU General Public License for more details.
 static int __init audio_q6_init(void)
 {
 	adsp_err_init();
-	audio_cal_init();
 	rtac_init();
 	adm_init();
 	afe_init();
--- techpack/audio/soc/soundwire.c
+++ techpack/audio/soc/soundwire.c
@@ -1026,7 +1026,7 @@ static int __init soundwire_init(void)
 
 	return retval;
 }
-module_init(soundwire_init);
+postcore_initcall(soundwire_init);
 module_exit(soundwire_exit);
 
 
