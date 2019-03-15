95b11cadbe77f895a8ab3d5201cb9c6f3a6991a2
Artem Borisov <dedsa2002@gmail.com>
2019-03-15 20:08:15 +0530

    Partially revert "input: ft5435: Fix the suspend/resume logic"
    * Apparently not every panel happy with that
    * Let's revert most of the changes, but keep the IRQ-related fixes
    
    This reverts commit 19f89492da6f2288b3a8a36d1b6fc89d66c87abc.
    
    Change-Id: I7b4540075f23523037047b97e5fb05706f421bd7

--- drivers/input/touchscreen/ft5435/ft5435_ts.c
+++ drivers/input/touchscreen/ft5435/ft5435_ts.c
@@ -1390,57 +1390,47 @@ static int ft5435_ts_pinctrl_select(struct ft5435_ts_data *ft5435_data,
 }
 
 #if defined(FOCALTECH_TP_GESTURE)
-static int ft_tp_suspend(struct ft5435_ts_data *data)
+static int  ft_tp_suspend(struct ft5435_ts_data *data)
 {
 	int i = 0;
 	u8 state = 0;
 
-	printk("[FTS] FTS_GESTURE suspend\n");
+	printk("[FTS] FTS_GESTRUE suspend\n");
+	ft5x0x_write_reg(data->client, 0xd0, 0x01);
+	ft5x0x_write_reg(data->client, 0xd1, 0xff);
+	ft5x0x_write_reg(data->client, 0xd2, 0xff);
+	ft5x0x_write_reg(data->client, 0xd5, 0xff);
+	ft5x0x_write_reg(data->client, 0xd6, 0xff);
+	ft5x0x_write_reg(data->client, 0xd7, 0xff);
+	ft5x0x_write_reg(data->client, 0xd8, 0xff);
+	msleep(10);
 
 	for (i = 0; i < 10; i++) {
-		ft5x0x_write_reg(data->client, 0xd0, 0x01);
-		ft5x0x_write_reg(data->client, 0xd1, 0xff);
-		ft5x0x_write_reg(data->client, 0xd2, 0xff);
-		ft5x0x_write_reg(data->client, 0xd5, 0xff);
-		ft5x0x_write_reg(data->client, 0xd6, 0xff);
-		ft5x0x_write_reg(data->client, 0xd7, 0xff);
-		ft5x0x_write_reg(data->client, 0xd8, 0xff);
-		msleep(10);
+		printk("[FTS]tpd_suspend4 %d\t", i);
 		ft5x0x_read_reg(data->client, 0xd0, &state);
-		if (state == 1)
+
+		if (state == 1) {
+			printk("[FTS]TPD gesture write 0x01 OK\n");
 			break;
+		} else {
+			ft5x0x_write_reg(data->client, 0xd0, 0x01);
+			ft5x0x_write_reg(data->client, 0xd1, 0xff);
+			ft5x0x_write_reg(data->client, 0xd2, 0xff);
+			ft5x0x_write_reg(data->client, 0xd5, 0xff);
+			ft5x0x_write_reg(data->client, 0xd6, 0xff);
+			ft5x0x_write_reg(data->client, 0xd7, 0xff);
+			ft5x0x_write_reg(data->client, 0xd8, 0xff);
+			msleep(10);
+		}
 	}
 
-	if (i >= 10)
+	if (i >= 9)
 	{
 		printk("[FTS]TPD gesture write 0x01 to d0 fail \n");
 	}
 
-	printk("[FTS] FTS_GESTURE suspend end\n");
-	return 0;
-}
-
-static int ft_tp_resume(struct ft5435_ts_data *data)
-{
-	int i = 0;
-	u8 state = 0;
-
-	printk("[FTS] FTS_GESTURE resume\n");
-
-	for (i = 0; i < 10; i++) {
-		ft5x0x_write_reg(data->client, 0xd0, 0x00);
-		msleep(10);
-		ft5x0x_read_reg(data->client, 0xd0, &state);
-		if (state == 0)
-			break;
-	}
-
-	if (i >= 10)
-	{
-		printk("[FTS]TPD resume write 0x00 to d0 fail \n");
-	}
-
-	printk("[FTS] FTS_GESTURE resume end\n");
+	data->suspended = true;
+	printk("[FTS] FTS_GESTRUE suspend end\n");
 	return 0;
 }
 #endif
@@ -1462,6 +1452,8 @@ static int ft5435_ts_suspend(struct device *dev)
 		return 0;
 	}
 
+	disable_irq(data->client->irq);
+
 	/* release all touches */
 	for (i = 0; i < data->pdata->num_max_touches; i++) {
 		input_mt_slot(data->input_dev, i);
@@ -1471,16 +1463,15 @@ static int ft5435_ts_suspend(struct device *dev)
 	input_sync(data->input_dev);
 
 #if defined(FOCALTECH_TP_GESTURE)
-	if (gesture_func_on) {
-		ft_tp_suspend(data);
-		enable_irq_wake(data->client->irq);
-		data->suspended = true;
-		return 0;
+	{
+		if (gesture_func_on) {
+			enable_irq(data->client->irq);
+			enable_irq_wake(data->client->irq);
+			ft_tp_suspend(data);
+			return 0;
+		}
 	}
 #endif
-
-	disable_irq(data->client->irq);
-
 	if (gpio_is_valid(data->pdata->reset_gpio)) {
 		gpio_set_value_cansleep(data->pdata->reset_gpio, 1);
 		msleep(300);
@@ -1507,6 +1498,8 @@ static int ft5435_ts_resume(struct device *dev)
 {
 	struct ft5435_ts_data *data = g_ft5435_ts_data;
 
+
+
 	if (!data->suspended) {
 		dev_dbg(dev, "Already in awake state\n");
 		return 0;
@@ -1515,15 +1508,6 @@ static int ft5435_ts_resume(struct device *dev)
 	/* release all touches */
 	input_mt_report_slot_state(data->input_dev, MT_TOOL_FINGER, 0);
 	input_sync(data->input_dev);
-
-#if defined(FOCALTECH_TP_GESTURE)
-	if (gesture_func_on) {
-		ft_tp_resume(data);
-		disable_irq_wake(data->client->irq);
-		data->suspended = false;
-		return 0;
-	}
-#endif
 
 /*hw rst*/
 	if (gpio_is_valid(data->pdata->reset_gpio)) {
@@ -1536,7 +1520,14 @@ static int ft5435_ts_resume(struct device *dev)
 
 
 	ft5x0x_write_reg(data->client, 0x8c, 0x01);
+#if defined(FOCALTECH_TP_GESTURE)
+	if (gesture_func_on)
+		disable_irq_wake(data->client->irq);
+	else
+		enable_irq(data->client->irq);
+#else
 	enable_irq(data->client->irq);
+#endif
 	data->suspended = false;
 
 #if defined(USB_CHARGE_DETECT)
