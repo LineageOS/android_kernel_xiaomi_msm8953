f6170082868f259e56af45156119efc5b7a15119
khusika <khusikadhamar@gmail.com>
2019-04-14 11:57:44 +0700

    drivers: power: Remove unused function 'soc_to_setpoint'
    Tissot never use this
    https://github.com/LineageOS/android_kernel_xiaomi_msm8953/blob/a61e0a55bd3f0ddb06662d805f7688106300d9a5/drivers/power/supply/qcom/qpnp-fg.c#L8059-L8064
    
    Change-Id: I7147aabac95e81d4910cd27a5e959a776359ec1e
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -2035,12 +2035,6 @@ static void fg_handle_battery_insertion(struct fg_chip *chip)
 	schedule_delayed_work(&chip->update_sram_data, msecs_to_jiffies(0));
 }
 
-
-static int soc_to_setpoint(int soc)
-{
-	return DIV_ROUND_CLOSEST(soc * 255, 100);
-}
-
 static void batt_to_setpoint_adc(int vbatt_mv, u8 *data)
 {
 	int val;
