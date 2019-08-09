941d9f008aca9346aa0a669307392314b8a982ec
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2019-08-09 21:03:55 +0530

    wlan: Remove IOCTL SET_PNO
    SET_PNO IOCTL is not getting used as there is already
    existing cfg_ops for this operation.
    As part of code cleanup remove the code related to
    this ioctl.
    
    Change-Id: I6fbae5929824f7948d757db92df42b6e62085581
    CRs-fixed: 2494309

--- CORE/HDD/inc/wlan_hdd_wext.h
+++ CORE/HDD/inc/wlan_hdd_wext.h
@@ -402,10 +402,6 @@ extern int iw_set_auth(struct net_device *dev,struct iw_request_info *info,
 extern int iw_get_auth(struct net_device *dev,struct iw_request_info *info,
                        union iwreq_data *wrqu,char *extra);
 
-VOS_STATUS iw_set_pno(struct net_device *dev, struct iw_request_info *info,
-                      union iwreq_data *wrqu, char *extra, int nOffset);
-
-
 VOS_STATUS iw_set_rssi_filter(struct net_device *dev, struct iw_request_info *info,
                               union iwreq_data *wrqu, char *extra, int nOffset);
 
