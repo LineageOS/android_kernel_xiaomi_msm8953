becc219c7f47e504285220de29901303c3640dd3
Takeshiro04 <gianashtonreyes@gmail.com>
2020-05-29 15:51:48 +0000

    Added CircleCI Script

---
+++ .circleci/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Cloning dependencies"
+git clone --depth=1 -b sx https://github.com/Takeshiro04/android_kernel_xiaomi_msm8953 kernel
+cd kernel
+git clone --depth=1 -b master https://github.com/kdrag0n/proton-clang clang
+git clone https://github.com/Takeshiro04/AnyKernel3 -b tissot --depth=1 AnyKernel
+echo "Done"
+KERNEL_DIR=$(pwd)
+IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
+TANGGAL=$(date +"%Y%m%d-%H")
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+export PATH="$(pwd)/clang/bin:$PATH"
+export KBUILD_COMPILER_STRING="$(kernel/clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
+export ARCH=arm64
+export KBUILD_BUILD_USER=Takeshiro04
+export KBUILD_BUILD_HOST=circleci
+# Compile plox
+function compile() {
+    make -j$(nproc) O=out ARCH=arm64 tissot_defconfig
+    make -j$(nproc) O=out \
+                    ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+
+    if ! [ -a "$IMAGE" ]; then
+        exit 1
+        echo "There are some issues"
+    fi
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Elucidator-Q-Kernel-${TANGGAL}.zip *
+    curl https://bashupload.com/Elucidator-Q-Kernel-${TANGGAL}.zip --data-binary @Elucidator-Q-Kernel-${TANGGAL}.zip
+}
+compile
+zipping
