918a5bc22f33aeec1cbda6f1d7e38ede6519bf86
Ramanasarvesh Sadula <rsadula@codeaurora.org>
2020-09-25 12:14:09 +0530

    wlan: Added support to check the kernel version.
    Added condtion to check the kernel version is greater than or equal
    to the 4.14 for wpalUnRegisterInterrupt in vos_watchdog_wlan_shutdown
    function.
    
    Change-Id: I8da32966f5ed7773b56a3d3305adf6dae4126fe7
    CRs-Fixed: 2785420

--- CORE/VOSS/src/vos_sched.c
+++ CORE/VOSS/src/vos_sched.c
@@ -2038,8 +2038,10 @@ VOS_STATUS vos_watchdog_wlan_shutdown(void)
        return VOS_STATUS_E_FAILURE;
     }
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0))
     wpalUnRegisterInterrupt(DXE_INTERRUPT_RX_READY);
     wpalUnRegisterInterrupt(DXE_INTERRUPT_TX_COMPLE);
+#endif
 
     /* Take the lock here */
     spin_lock(&gpVosWatchdogContext->wdLock);
