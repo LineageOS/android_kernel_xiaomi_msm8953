1aa858c3f15ef93f916c5411023c484b741a8c7f
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-22 23:06:53 -0700

    fs: proc: Add PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
    For Android 9 launch A/B devices migrating to Android 10 style
    system-as-root, `androidboot.force_normal_boot=1` must be passed in
    cmdline when booting into normal or charger mode. However, it is not
    always possible for one to modify the bootloader to adhere to these
    changes. As a workaround, one can use the presence of the
    `skip_initramfs` flag in cmdline to to decide whether to append the new
    flag to cmdline on the kernel side.
    
    Co-authored-by: jabashque <jabashque@gmail.com>
    Change-Id: Ia00ea2c54e2a7d2275e552837039033adb98d0ff

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,37 +2,51 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
+
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+    defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
+#define INITRAMFS_STR_FIND "skip_initramf"
 #endif
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-#define INITRAMFS_STR_FIND "skip_initramf"
 #define INITRAMFS_STR_REPLACE "want_initramf"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+#endif
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+#define ANDROID_FORCE_NORMAL_BOOT_STR "androidboot.force_normal_boot=1"
+#endif
 
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void) {
 	char *offset_addr;
+	char *proc_command_line_tail;
 
 	strcpy(proc_command_line, saved_command_line);
 
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
-
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
-}
+	if (offset_addr)
+		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
 #endif
+
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+	if (strstr(saved_command_line, INITRAMFS_STR_FIND)) {
+		// point proc_command_line_tail to the null terminator of the cmdline
+		proc_command_line_tail = proc_command_line + strlen(proc_command_line);
+		memcpy(proc_command_line_tail, " ", 1);
+		memcpy(proc_command_line_tail + 1, ANDROID_FORCE_NORMAL_BOOT_STR,
+                        sizeof(ANDROID_FORCE_NORMAL_BOOT_STR));
+	}
+#endif
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_printf(m, "%s\n", proc_command_line);
-#else
-	seq_printf(m, "%s\n", saved_command_line);
-#endif
 	return 0;
 }
 
@@ -50,9 +64,7 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	proc_command_line_init();
-#endif
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
